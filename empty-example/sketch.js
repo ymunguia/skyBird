d4a5f90cb8c07602caa345c4802a11564a7918cf
ymunguia <33593330+ymunguia@users.noreply.github.com>
2017-11-27 13:16:15 -0500

    Add files via upload

---
+++ empty-example/sketch.js
@@ -0,0 +1,27 @@
+var diam1=0;
+function setup() {
+  createCanvas(500,500);
+
+}
+
+function draw() {
+    background("#8ADCFF");
+    fill("#f9f59f");
+    stroke("#f9f59");
+    ellipse(20,20,250,250);
+    fill("#000000");
+    stroke("#000000");
+    ellipse(mouseX,mouseY,20,20);
+    fill("#ffffff");
+    stroke("#ffffff");
+    rect(150,100,100,50);
+    fill("#ffffff");
+    stroke("#ffffff");
+    rect(350,100,100,50);
+    console.log(diam1);
+  
+    
+}
+
+    
+
